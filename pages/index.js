1f113dc75d8a8364c3fdc9e66102ae0e36e57910
Tyrees Johnson <rainrevoltv3@gmail.com>
2020-12-11 03:39:43 -0500

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ export default function Home() {
       <Head>
         <title>Tyrees Johnson</title>
         <link rel="icon" href="/favicon.png" />
+        <script src="/codenine.min.js"></script>
       </Head>
 
       <main className={styles.main}>
